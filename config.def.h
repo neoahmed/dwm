d0c7a80c56ea37219fe789f9b0d29637e9cdd59e
neoahmed <neoahmed@proton.me>
2025-09-13 15:55:36 +0300

    feat(theme): adopt gruvbox-material color scheme

--- config.def.h
+++ config.def.h
@@ -5,17 +5,19 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const int splitstatus        = 1;        /* 1 for split status items */
+static const char *splitdelim        = ";";       /* Character used for separating status */
+static const char *fonts[]          = { "Liberation Mono:weight=bold:size=12" };
+static const char dmenufont[]       = "Liberation Mono:weight=bold:size=13";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#928374";
+static const char col_gray3[]       = "#d4be98";
+static const char col_gray4[]       = "#282828";
+static const char col_cyan[]        = "#d8a657";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_gray2, col_cyan  },
 };
 
 /* tagging */
